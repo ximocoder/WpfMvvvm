668140ebb194f3e01e383b3d1b855c7e2db21c26
Ximo <ximo.coder@gmail.com>
2018-04-30 22:43:08 +0200

    Let there be light.

---
+++ SubtitleDownloader/Models/Subtitle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleDownloader.Models
+{
+    public class Subtitle
+    {
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+
+        public Subtitle()
+        {
+            this.Name = "test";
+            this.Language = "ES";
+            this.Type = "sub";
+            this.Content = "bla bla bla bla";
+        }
+    }
+}
---
+++ SubtitleDownloader/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using SubtitleDownloader.Models;
+
+namespace SubtitleDownloader.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool canExecute = true;
+
+
+        public bool CanExecute
+        {
+            get
+            {
+                return this.canExecute;
+            }
+
+            set
+            {
+                if (this.canExecute == value)
+                {
+                    return;
+                }
+
+                this.canExecute = value;
+            }
+        }
+
+        private ICommand searchCommand;
+        public ICommand SearchCommand
+        {
+            get => searchCommand;
+            set => searchCommand = value;
+        }
+
+        private string _subtitleContent;
+        public string SubtitleContent
+        {
+            get => _subtitleContent;
+            set
+            {
+                _subtitleContent = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public MainWindowViewModel()
+        {
+            SubtitleContent = string.Empty;
+            searchCommand = new RelayCommand(SearchSubtitle, param => this.canExecute);
+        }
+
+        private void SearchSubtitle(object obj)
+        {
+            var content = new Subtitle().Content;
+            SubtitleContent = content;
+        }
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
